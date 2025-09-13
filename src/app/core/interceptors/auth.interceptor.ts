0e8f48cb535a1064679c7c1f3db154d5f3a45d15
Leonardo Benitez <>
2025-09-12 23:16:16 -0500

    config deploy

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -3,15 +3,16 @@ import { inject } from '@angular/core';
 import { TokenService } from '../services/token.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const tokenService = inject(TokenService);
-  const token = tokenService.getToken();
+    const tokenService = inject(TokenService);
+    const token = tokenService.getToken();
 
-  if (token) {
-    const cloned = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${token}`)
-    });
-    return next(cloned);
-  }
+    if (token) {
+        const cloned = req.clone({
+            headers: req.headers.set('Authorization', `Bearer ${token}`),
+            withCredentials: true
+        });
+        return next(cloned);
+    }
 
-  return next(req);
+    return next(req);
 };
